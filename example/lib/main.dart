a6258b46bb2e494b8acb8a8a844ab40172e8b2d4
Shun Kakinoki <shunkakinoki@gmail.com>
2019-06-03 18:12:35 +0900

    added colors etc...

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -9,26 +9,28 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return new MaterialApp(
         home: new Scaffold(
-          appBar: new AppBar(
-            brightness: Brightness.light,
-            backgroundColor: Colors.transparent,
-            centerTitle: true,
-            elevation: 0.0,
-            title: new Text(
-              'Gradient Image',
-              style: TextStyle(color: Colors.grey, fontWeight: FontWeight.w300),
-            ),
-          ),
-          backgroundColor: Colors.white,
-          body: Center(
-            child: Container(
+      appBar: new AppBar(
+        brightness: Brightness.light,
+        backgroundColor: Colors.transparent,
+        centerTitle: true,
+        elevation: 0.0,
+        title: new Text(
+          'Gradient Image',
+          style: TextStyle(color: Colors.grey, fontWeight: FontWeight.w300),
+        ),
+      ),
+      backgroundColor: Colors.white,
+      body: Center(
+        child: Container(
+          child: GradientImage(
               height: 300,
               width: 300,
-              child: GradientImage(
-                  image: NetworkImage(
-                      'https://www.gstatic.com/webp/gallery3/1.png')),
-            ),
-          ),
-        ));
+              startColor: Colors.lightBlue.withOpacity(0),
+              endColor: Colors.blue.withOpacity(0.4),
+              image:
+                  NetworkImage('https://www.gstatic.com/webp/gallery3/1.png')),
+        ),
+      ),
+    ));
   }
 }
