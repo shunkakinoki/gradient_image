ebbad41e22642bbddab7fb6d7a1bf6ed7efc8e49
Shun Kakinoki <shunkakinoki@gmail.com>
2019-06-03 18:47:15 +0900

    updated test

--- test/gradient_image_test.dart
+++ test/gradient_image_test.dart
@@ -1,9 +1,1 @@
-import 'package:flutter_test/flutter_test.dart';
-
-import 'package:gradient_image/gradient_image.dart';
-
-void main() {
-  test('adds one to input values', () {
-
-  });
-}
+void main() {}
