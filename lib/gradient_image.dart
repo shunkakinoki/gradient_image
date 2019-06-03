1b958d0897bdef80726c51299883ae2ca1564731
Shun Kakinoki <shunkakinoki@gmail.com>
2019-06-03 17:10:48 +0900

    created example dir

--- lib/gradient_image.dart
+++ lib/gradient_image.dart
@@ -1,7 +1,54 @@
 library gradient_image;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
+import 'package:flutter/material.dart';
+
+class GradientImage extends StatelessWidget {
+  GradientImage(
+      {@required this.image,
+      @required this.height,
+      this.width,
+      this.gradient,
+      this.startColor,
+      this.endColor});
+
+  final ImageProvider image;
+  final double height;
+  final double width;
+  final bool gradient;
+  final Color startColor;
+  final Color endColor;
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(children: <Widget>[
+      Container(
+          height: height,
+          width: double.infinity,
+          child: DecoratedBox(
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                fit: BoxFit.cover,
+                image: image,
+              ),
+              // ...
+            ),
+          )),
+      Container(
+        height: height,
+        decoration: BoxDecoration(
+            color: Colors.white,
+            gradient: LinearGradient(
+                begin: FractionalOffset.topCenter,
+                end: FractionalOffset.bottomCenter,
+                colors: [
+                  startColor,
+                  endColor,
+                ],
+                stops: [
+                  0.0,
+                  1.0
+                ])),
+      )
+    ]);
+  }
 }
